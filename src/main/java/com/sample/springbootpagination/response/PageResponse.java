7f647169da76931d1c53950ce64cdec6dc42b86f
saisampaths60 <saisampathkatru@gmail.com>
2024-05-22 23:12:46 +0530

    page response generic

---
+++ src/main/java/com/sample/springbootpagination/response/PageResponse.java
@@ -0,0 +1,21 @@
+package com.sample.springbootpagination.response;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PageResponse<T> {
+
+	private List<T> content;
+	private String nextUrl;
+
+}
